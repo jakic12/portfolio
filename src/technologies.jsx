6f8202a05889fa52950467bb34d090d8eb141390
jakob <drusany.jakob@gmail.com>
2019-11-02 20:35:13 +0100

    Added list of technologies

---
+++ src/technologies.jsx
@@ -0,0 +1,709 @@
+import Amazonwebservices from "react-devicon/amazonwebservices/original";
+import Android from "react-devicon/android/original";
+import Angularjs from "react-devicon/angularjs/original";
+import Apache from "react-devicon/apache/original";
+import Appcelerator from "react-devicon/appcelerator/original";
+import Apple from "react-devicon/apple/original";
+import Atom from "react-devicon/atom/original";
+import Babel from "react-devicon/babel/original";
+import Backbonejs from "react-devicon/backbonejs/original";
+import Behance from "react-devicon/behance/original";
+import Bitbucket from "react-devicon/bitbucket /original";
+import Bootstrap from "react-devicon/bootstrap /original";
+import Bower from "react-devicon/bower/original";
+import C from "react-devicon/c/original";
+import Cakephp from "react-devicon/cakephp/original";
+import Ceylon from "react-devicon/ceylon/original";
+import Chrome from "react-devicon/chrome/original";
+import Codeigniter from "react-devicon/codeigniter /original";
+import Coffeescript from "react-devicon/coffeescript/original";
+import Confluence from "react-devicon/confluence/original";
+import Couchdb from "react-devicon/couchdb/original";
+import Cplusplus from "react-devicon/cplusplus/original";
+import Csharp from "react-devicon/csharp/original";
+import Css3 from "react-devicon/css3/original";
+import Cucumber from "react-devicon/cucumber/original";
+import D3js from "react-devicon/d3js/original";
+import Debian from "react-devicon/debian/original";
+import Devicon from "react-devicon/devicon/original";
+import Django from "react-devicon/django/original";
+import Docker from "react-devicon/docker/original";
+import Doctrine from "react-devicon/doctrine/original";
+import DotNet from "react-devicon/dot-net/original";
+import Drupal from "react-devicon/drupal/original";
+import Electron from "react-devicon/electron/original";
+import Elm from "react-devicon/elm/original";
+import Ember from "react-devicon/ember/original";
+import Erlang from "react-devicon/erlang/original";
+import Express from "react-devicon/express/original";
+import Facebook from "react-devicon/facebook/original";
+import Firefox from "react-devicon/firefox/original";
+import Foundation from "react-devicon/foundation/original";
+import Gatling from "react-devicon/gatling/original";
+import Gimp from "react-devicon/gimp/original";
+import Git from "react-devicon/git/original";
+import Github from "react-devicon/github/original";
+import Gitlab from "react-devicon/gitlab/original";
+import Go from "react-devicon/go/original";
+import Google from "react-devicon/google/original";
+import Gradle from "react-devicon/gradle/original";
+import Grunt from "react-devicon/grunt/original";
+import Gulp from "react-devicon/gulp/original";
+import Handlebars from "react-devicon/handlebars/original";
+import Heroku from "react-devicon/heroku/original";
+import Html5 from "react-devicon/html5/original";
+import Ie10 from "react-devicon/ie10/original";
+import Illustrator from "react-devicon/illustrator/original";
+import Inkscape from "react-devicon/inkscape/original";
+import Intellij from "react-devicon/intellij/original";
+import Ionic from "react-devicon/ionic/original";
+import Jasmine from "react-devicon/jasmine/original";
+import Java from "react-devicon/java/original";
+import Javascript from "react-devicon/javascript/original";
+import Jeet from "react-devicon/jeet/original";
+import Jetbrains from "react-devicon/jetbrains/original";
+import Jquery from "react-devicon/jquery/original";
+import Krakenjs from "react-devicon/krakenjs/original";
+import Laravel from "react-devicon/laravel/original";
+import Less from "react-devicon/less/original";
+import LICENSE from "react-devicon/LICENSE/original";
+import Linkedin from "react-devicon/linkedin/original";
+import Linux from "react-devicon/linux/original";
+import Meteor from "react-devicon/meteor/original";
+import Mocha from "react-devicon/mocha/original";
+import Mongodb from "react-devicon/mongodb/original";
+import Moodle from "react-devicon/moodle/original";
+import Mysql from "react-devicon/mysql/original";
+import Nginx from "react-devicon/nginx/original";
+import Nodejs from "react-devicon/nodejs/original";
+import Nodewebkit from "react-devicon/nodewebkit/original";
+import Npm from "react-devicon/npm/original";
+import Oracle from "react-devicon/oracle/original";
+import Photoshop from "react-devicon/photoshop/original";
+import Php from "react-devicon/php/original";
+import Phpstorm from "react-devicon/phpstorm/original";
+import Postgresql from "react-devicon/postgresql/original";
+import Protractor from "react-devicon/protractor/original";
+import Pycharm from "react-devicon/pycharm/original";
+import Python from "react-devicon/python/original";
+import Rails from "react-devicon/rails/original";
+import React from "react-devicon/react/original";
+import Redhat from "react-devicon/redhat/original";
+import Redis from "react-devicon/redis/original";
+import Ruby from "react-devicon/ruby/original";
+import Rubymine from "react-devicon/rubymine/original";
+import Safari from "react-devicon/safari/original";
+import Sass from "react-devicon/sass/original";
+import Sequelize from "react-devicon/sequelize/original";
+import Sketch from "react-devicon/sketch/original";
+import Slack from "react-devicon/slack/original";
+import Sourcetree from "react-devicon/sourcetree/original";
+import Ssh from "react-devicon/ssh/original";
+import Stylus from "react-devicon/stylus/original";
+import Swift from "react-devicon/swift/original";
+import Symfony from "react-devicon/symfony/original";
+import Tomcat from "react-devicon/tomcat/original";
+import Travis from "react-devicon/travis/original";
+import Trello from "react-devicon/trello/original";
+import Twitter from "react-devicon/twitter/original";
+import Typescript from "react-devicon/typescript/original";
+import Ubuntu from "react-devicon/ubuntu/original";
+import Vagrant from "react-devicon/vagrant/original";
+import Vim from "react-devicon/vim/original";
+import Visualstudio from "react-devicon/visualstudio/original";
+import Vuejs from "react-devicon/vuejs/original";
+import Webpack from "react-devicon/webpack/original";
+import Webstorm from "react-devicon/webstorm/original";
+import Windows8 from "react-devicon/windows8/original";
+import Wordpress from "react-devicon/wordpress/original";
+import Yarn from "react-devicon/yarn/original";
+import Yii from "react-devicon/yii/original";
+import Zend from "react-devicon/zend/original";
+
+export default {
+  amazonwebservices: {
+    icon: Amazonwebservices,
+    label: "Amazonwebservices",
+    link: "https://aws.amazon.com/"
+  },
+  android: {
+    icon: Android,
+    label: "Android",
+    link: "https://www.android.com/"
+  },
+  angularjs: {
+    icon: Angularjs,
+    label: "Angularjs",
+    link: "https://angularjs.org/"
+  },
+  apache: {
+    icon: Apache,
+    label: "Apache",
+    link: "https://httpd.apache.org/"
+  },
+  appcelerator: {
+    icon: Appcelerator,
+    label: "Appcelerator",
+    link: "https://www.appcelerator.com/"
+  },
+  apple: {
+    icon: Apple,
+    label: "Apple",
+    link: "https://www.apple.com/"
+  },
+  atom: {
+    icon: Atom,
+    label: "Atom",
+    link: "https://atom.io/"
+  },
+  babel: {
+    icon: Babel,
+    label: "Babel",
+    link: "https://babeljs.io/"
+  },
+  backbonejs: {
+    icon: Backbonejs,
+    label: "Backbonejs",
+    link: "https://backbonejs.org/"
+  },
+  behance: {
+    icon: Behance,
+    label: "Behance",
+    link: "https://www.behance.net/"
+  },
+  bitbucket: {
+    icon: Bitbucket,
+    label: "Bitbucket",
+    link: "https://bitbucket.org/"
+  },
+  bootstrap: {
+    icon: Bootstrap,
+    label: "Bootstrap",
+    link: "https://getbootstrap.com/"
+  },
+  bower: {
+    icon: Bower,
+    label: "Bower",
+    link: "https://bower.io/"
+  },
+  c: {
+    icon: C,
+    label: "C",
+    link: "https://en.cppreference.com/w/"
+  },
+  cakephp: {
+    icon: Cakephp,
+    label: "Cakephp",
+    link: "https://cakephp.org/"
+  },
+  ceylon: {
+    icon: Ceylon,
+    label: "Ceylon",
+    link: "https://ceylon-lang.org/"
+  },
+  chrome: {
+    icon: Chrome,
+    label: "Chrome",
+    link: "https://www.google.com/intl/sl_SI/chrome/"
+  },
+  codeigniter: {
+    icon: Codeigniter,
+    label: "Codeigniter",
+    link: "https://codeigniter.com/"
+  },
+  coffeescript: {
+    icon: Coffeescript,
+    label: "Coffeescript",
+    link: "https://coffeescript.org/"
+  },
+  confluence: {
+    icon: Confluence,
+    label: "Confluence",
+    link: "https://www.atlassian.com/software/confluence"
+  },
+  couchdb: {
+    icon: Couchdb,
+    label: "Couchdb",
+    link: "http://couchdb.apache.org/"
+  },
+  cplusplus: {
+    icon: Cplusplus,
+    label: "Cplusplus",
+    link: "http://www.cplusplus.com/"
+  },
+  csharp: {
+    icon: Csharp,
+    label: "Csharp",
+    link: "https://en.wikipedia.org/wiki/C_Sharp_(programming_language)"
+  },
+  css3: {
+    icon: Css3,
+    label: "Css3",
+    link: "http://www.css3.info/"
+  },
+  cucumber: {
+    icon: Cucumber,
+    label: "Cucumber",
+    link: "https://cucumber.io/"
+  },
+  d3js: {
+    icon: D3js,
+    label: "D3js",
+    link: "https://d3js.org/"
+  },
+  debian: {
+    icon: Debian,
+    label: "Debian",
+    link: "https://www.debian.org/"
+  },
+  devicon: {
+    icon: Devicon,
+    label: "Devicon",
+    link: "https://konpa.github.io/devicon/"
+  },
+  django: {
+    icon: Django,
+    label: "Django",
+    link: "https://www.djangoproject.com/"
+  },
+  docker: {
+    icon: Docker,
+    label: "Docker",
+    link: "https://www.docker.com/"
+  },
+  doctrine: {
+    icon: Doctrine,
+    label: "Doctrine",
+    link: "https://www.doctrine-project.org/"
+  },
+  dotnet: {
+    icon: DotNet,
+    label: "DotNet",
+    link: "https://dotnet.microsoft.com/"
+  },
+  drupal: {
+    icon: Drupal,
+    label: "Drupal",
+    link: "https://www.drupal.org/"
+  },
+  electron: {
+    icon: Electron,
+    label: "Electron",
+    link: "https://electronjs.org/"
+  },
+  elm: {
+    icon: Elm,
+    label: "Elm",
+    link: "https://elm-lang.org/"
+  },
+  ember: {
+    icon: Ember,
+    label: "Ember",
+    link: "https://emberjs.com/"
+  },
+  erlang: {
+    icon: Erlang,
+    label: "Erlang",
+    link: "https://www.erlang.org/"
+  },
+  express: {
+    icon: Express,
+    label: "Express",
+    link: "https://expressjs.com/"
+  },
+  firefox: {
+    icon: Firefox,
+    label: "Firefox",
+    link: "https://www.mozilla.org/sl/firefox/new/"
+  },
+  foundation: {
+    icon: Foundation,
+    label: "Foundation",
+    link: "https://foundation.zurb.com/"
+  },
+  gatling: {
+    icon: Gatling,
+    label: "Gatling",
+    link: "https://gatling.io/"
+  },
+  gimp: {
+    icon: Gimp,
+    label: "Gimp",
+    link: "https://www.gimp.org/"
+  },
+  git: {
+    icon: Git,
+    label: "Git",
+    link: "https://git-scm.com/"
+  },
+  github: {
+    icon: Github,
+    label: "Github",
+    link: "https://github.com/"
+  },
+  gitlab: {
+    icon: Gitlab,
+    label: "Gitlab",
+    link: "https://gitlab.com/"
+  },
+  go: {
+    icon: Go,
+    label: "Go",
+    link: "https://golang.org/"
+  },
+  google: {
+    icon: Google,
+    label: "Google",
+    link: "https://www.google.si/"
+  },
+  gradle: {
+    icon: Gradle,
+    label: "Gradle",
+    link: "https://gradle.org/"
+  },
+  grunt: {
+    icon: Grunt,
+    label: "Grunt",
+    link: "https://gruntjs.com/"
+  },
+  gulp: {
+    icon: Gulp,
+    label: "Gulp",
+    link: "https://gulpjs.com/"
+  },
+  handlebars: {
+    icon: Handlebars,
+    label: "Handlebars",
+    link: "https://handlebarsjs.com/"
+  },
+  heroku: {
+    icon: Heroku,
+    label: "Heroku",
+    link: "https://www.heroku.com/"
+  },
+  html5: {
+    icon: Html5,
+    label: "Html5",
+    link: "https://en.wikipedia.org/wiki/HTML5"
+  },
+  illustrator: {
+    icon: Illustrator,
+    label: "Illustrator",
+    link: "https://www.adobe.com/si/products/illustrator.html"
+  },
+  inkscape: {
+    icon: Inkscape,
+    label: "Inkscape",
+    link: "https://inkscape.org/"
+  },
+  intellij: {
+    icon: Intellij,
+    label: "Intellij",
+    link: "https://www.jetbrains.com/idea/"
+  },
+  ionic: {
+    icon: Ionic,
+    label: "Ionic",
+    link: "https://ionicframework.com/"
+  },
+  jasmine: {
+    icon: Jasmine,
+    label: "Jasmine",
+    link: "https://jasmine.github.io/"
+  },
+  java: {
+    icon: Java,
+    label: "Java",
+    link: "https://www.java.com/download/"
+  },
+  javascript: {
+    icon: Javascript,
+    label: "Javascript",
+    link: "https://www.javascript.com/"
+  },
+  jeet: {
+    icon: Jeet,
+    label: "Jeet",
+    link: "http://jeet.gs/"
+  },
+  jetbrains: {
+    icon: Jetbrains,
+    label: "Jetbrains",
+    link: "https://www.jetbrains.com/"
+  },
+  jquery: {
+    icon: Jquery,
+    label: "Jquery",
+    link: "https://jquery.com/"
+  },
+  krakenjs: {
+    icon: Krakenjs,
+    label: "Krakenjs",
+    link: "http://krakenjs.com/"
+  },
+  laravel: {
+    icon: Laravel,
+    label: "Laravel",
+    link: "https://laravel.com/"
+  },
+  less: {
+    icon: Less,
+    label: "Less",
+    link: "http://lesscss.org/"
+  },
+  linkedin: {
+    icon: Linkedin,
+    label: "Linkedin",
+    link: "https://www.linkedin.com/"
+  },
+  linux: {
+    icon: Linux,
+    label: "Linux",
+    link: "https://www.linux.org/"
+  },
+  meteor: {
+    icon: Meteor,
+    label: "Meteor",
+    link: "https://www.meteor.com/"
+  },
+  mocha: {
+    icon: Mocha,
+    label: "Mocha",
+    link: "https://mochajs.org/"
+  },
+  mongodb: {
+    icon: Mongodb,
+    label: "Mongodb",
+    link: "https://www.mongodb.com/"
+  },
+  moodle: {
+    icon: Moodle,
+    label: "Moodle",
+    link: "https://moodle.com/"
+  },
+  mysql: {
+    icon: Mysql,
+    label: "Mysql",
+    link: "https://www.mysql.com/"
+  },
+  nginx: {
+    icon: Nginx,
+    label: "Nginx",
+    link: "https://www.nginx.com/"
+  },
+  nodejs: {
+    icon: Nodejs,
+    label: "Nodejs",
+    link: "https://nodejs.org/"
+  },
+  nodewebkit: {
+    icon: Nodewebkit,
+    label: "Nodewebkit",
+    link: "https://nwjs.io/"
+  },
+  npm: {
+    icon: Npm,
+    label: "Npm",
+    link: "https://www.npmjs.com/"
+  },
+  oracle: {
+    icon: Oracle,
+    label: "Oracle",
+    link: "https://www.oracle.com/index.html"
+  },
+  photoshop: {
+    icon: Photoshop,
+    label: "Photoshop",
+    link: "https://www.photoshop.com/"
+  },
+  php: {
+    icon: Php,
+    label: "Php",
+    link: "https://www.php.net/"
+  },
+  phpstorm: {
+    icon: Phpstorm,
+    label: "Phpstorm",
+    link: "https://www.jetbrains.com/phpstorm/"
+  },
+  postgresql: {
+    icon: Postgresql,
+    label: "Postgresql",
+    link: "https://www.postgresql.org/"
+  },
+  protractor: {
+    icon: Protractor,
+    label: "Protractor",
+    link: "https://www.protractortest.org/"
+  },
+  pycharm: {
+    icon: Pycharm,
+    label: "Pycharm",
+    link: "https://www.jetbrains.com/pycharm/"
+  },
+  python: {
+    icon: Python,
+    label: "Python",
+    link: "https://www.python.org/"
+  },
+  rails: {
+    icon: Rails,
+    label: "Rails",
+    link: "https://rubyonrails.org/"
+  },
+  react: {
+    icon: React,
+    label: "React",
+    link: "https://reactjs.org/"
+  },
+  redhat: {
+    icon: Redhat,
+    label: "Redhat",
+    link: "https://www.redhat.com/en"
+  },
+  redis: {
+    icon: Redis,
+    label: "Redis",
+    link: "https://redis.io/"
+  },
+  ruby: {
+    icon: Ruby,
+    label: "Ruby",
+    link: "https://www.ruby-lang.org/en/"
+  },
+  rubymine: {
+    icon: Rubymine,
+    label: "Rubymine",
+    link: "https://www.jetbrains.com/ruby/"
+  },
+  safari: {
+    icon: Safari,
+    label: "Safari",
+    link: "https://www.apple.com/safari/"
+  },
+  sass: {
+    icon: Sass,
+    label: "Sass",
+    link: "https://sass-lang.com/"
+  },
+  sequelize: {
+    icon: Sequelize,
+    label: "Sequelize",
+    link: "https://sequelize.org/"
+  },
+  sketch: {
+    icon: Sketch,
+    label: "Sketch",
+    link: "https://www.sketch.com/"
+  },
+  slack: {
+    icon: Slack,
+    label: "Slack",
+    link: "https://slack.com/"
+  },
+  sourcetree: {
+    icon: Sourcetree,
+    label: "Sourcetree",
+    link: "https://www.sourcetreeapp.com/"
+  },
+  ssh: {
+    icon: Ssh,
+    label: "Ssh",
+    link: "https://www.ssh.com/"
+  },
+  stylus: {
+    icon: Stylus,
+    label: "Stylus",
+    link: "http://stylus-lang.com/"
+  },
+  swift: {
+    icon: Swift,
+    label: "Swift",
+    link: "https://www.swift.com/"
+  },
+  symfony: {
+    icon: Symfony,
+    label: "Symfony",
+    link: "https://symfony.com/"
+  },
+  tomcat: {
+    icon: Tomcat,
+    label: "Tomcat",
+    link: "http://tomcat.apache.org/"
+  },
+  travis: {
+    icon: Travis,
+    label: "Travis",
+    link: "https://travis-ci.org/"
+  },
+  trello: {
+    icon: Trello,
+    label: "Trello",
+    link: "https://trello.com/"
+  },
+  twitter: {
+    icon: Twitter,
+    label: "Twitter",
+    link: "https://twitter.com/"
+  },
+  typescript: {
+    icon: Typescript,
+    label: "Typescript",
+    link: "https://www.typescriptlang.org/"
+  },
+  ubuntu: {
+    icon: Ubuntu,
+    label: "Ubuntu",
+    link: "https://ubuntu.com/"
+  },
+  vagrant: {
+    icon: Vagrant,
+    label: "Vagrant",
+    link: "https://www.vagrantup.com/"
+  },
+  vim: {
+    icon: Vim,
+    label: "Vim",
+    link: "https://www.vim.org/"
+  },
+  visualstudio: {
+    icon: Visualstudio,
+    label: "Visualstudio",
+    link: "https://visualstudio.microsoft.com/"
+  },
+  vuejs: {
+    icon: Vuejs,
+    label: "Vuejs",
+    link: "https://vuejs.org/"
+  },
+  webpack: {
+    icon: Webpack,
+    label: "Webpack",
+    link: "https://webpack.js.org/"
+  },
+  webstorm: {
+    icon: Webstorm,
+    label: "Webstorm",
+    link: "https://www.jetbrains.com/webstorm/"
+  },
+  wordpress: {
+    icon: Wordpress,
+    label: "Wordpress",
+    link: "https://wordpress.com/"
+  },
+  yarn: {
+    icon: Yarn,
+    label: "Yarn",
+    link: "https://yarnpkg.com/"
+  },
+  yii: {
+    icon: Yii,
+    label: "Yii",
+    link: "https://www.yiiframework.com/"
+  },
+  zend: {
+    icon: Zend,
+    label: "Zend",
+    link: "https://www.zend.com/"
+  }
+};
