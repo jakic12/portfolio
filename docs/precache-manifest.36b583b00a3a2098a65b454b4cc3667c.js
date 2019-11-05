609fb64ba3b2c0c20407f3da640cb591d13567bb
jakob <drusany.jakob@gmail.com>
2019-11-05 23:21:33 +0100

    Build v1

---
+++ docs/precache-manifest.36b583b00a3a2098a65b454b4cc3667c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba5725c55f8ba765f65749fdd8a5e675",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62571e44be271a3935df",
+    "url": "/static/css/main.d7e578fa.chunk.css"
+  },
+  {
+    "revision": "8cad3b29a91a3d3e3679",
+    "url": "/static/js/2.14872033.chunk.js"
+  },
+  {
+    "revision": "62571e44be271a3935df",
+    "url": "/static/js/main.81ef4c0d.chunk.js"
+  },
+  {
+    "revision": "05129d9571d833ef1980",
+    "url": "/static/js/runtime-main.a5eaf332.js"
+  }
+]);
